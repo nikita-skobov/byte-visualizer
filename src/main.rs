600ab0530205f2a62b7cb8a41e3064be59d0614f
nikita-skobov <nskobov.websites@gmail.com>
2019-05-29 17:39:02 -0500

    starts with working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,146 @@
+extern crate png_encode_mini;
+
+use std::path::Path;
+use std::fs;
+use std::env;
+use std::process;
+
+fn get_output_name(file_path: &str) -> String {
+  let path = Path::new(file_path);
+
+  let os_str = match path.file_name() {
+    None => panic!("cannot find a filename to output from {}", file_path),
+    Some(s) => s,
+  };
+
+  let mut file_name = match os_str.to_str() {
+    None => panic!("cannot convert file path to string from {}", file_path),
+    Some(s) => String::from(s),
+  };
+
+  file_name.push_str(".png");
+  file_name
+}
+
+fn get_args() -> (String, String) {
+  let arg_vec: Vec<String> = env::args().collect();
+
+  if arg_vec.len() < 2 {
+    println!("Must provide a file to read");
+    process::exit(1);
+  }
+
+  let file_path = arg_vec[1].clone();
+  let output_name = get_output_name(&file_path[..]);
+
+  (file_path, output_name)
+}
+
+fn get_file_as_binary(file_path: &str) -> Vec<u8> {
+  let contents = match fs::read(file_path) {
+    Ok(d) => d,
+    Err(e) => panic!("Failed to read file: {} \n Error: \n {}", file_path, e),
+  };
+
+  contents
+}
+
+fn get_dimensions_from_len(len: f64) -> (u32, u32) {
+  let sqrt = len.sqrt();
+  let mut upper_limit = sqrt as u32;
+
+  if upper_limit * upper_limit < len as u32 {
+    upper_limit += 1;
+  }
+
+  (upper_limit, upper_limit)
+}
+
+fn fill_image_data(
+  binary_data: &Vec<u8>,
+  width: i32,
+  height: i32,
+  values_per_pixel: i32,
+) -> Vec<u8> {
+  let num_pixels = width * height;
+  let vector_size = num_pixels * values_per_pixel;
+  let mut image_data: Vec<u8> = vec![0; vector_size as usize];
+
+  let mut index = 0;
+  let mut height_level = height - 1;
+
+  let width_offset = values_per_pixel * width;
+  let mut height_offset = height - (2 * height_level) - 1;
+  let mut iterator_offset = width_offset * height_offset;
+
+  for i in 0..image_data.len() {
+    let iterator = i as i32;
+    // the iterator corresponds to bytes, whereas the index
+    // corresponds to pixels. a pixel has 3 bytes (if RGB) or 4 bytes (if RGBA).
+    // every time the iterator surpasses values_per_pixel, we increment the index.
+
+    if iterator % values_per_pixel == 0 {
+      let red_value = if index < binary_data.len() { binary_data[index] } else { 0 };
+      
+
+      // every time the index surpasses the width, we go down one height level
+      if index > 1 && index as i32 % width == 0 {
+        height_level -= 1;
+
+        // we recalculate height offset, and iterator offset
+        // only when the height level changes
+        height_offset = height - (2 * height_level) - 1;
+        iterator_offset = width_offset * height_offset;
+      }
+
+      let pixel_index = iterator - iterator_offset;
+      let pixel_index: usize = pixel_index as usize;
+      // png_encode_mini takes an array of u8 where
+      // every 3 or 4 elements represents a pixel
+      // and outputs in the following order:
+      // [
+      //   6  7  8
+      //   3  4  5
+      //   0  1  2
+      // ]
+      // but we want:
+      // [
+      //   0  1  2
+      //   3  4  5
+      //   6  7  8
+      // ]
+      // so we calculate a pixel index which is based off an offset
+      // from the height level and the width.
+
+      image_data[pixel_index] = 0; // R
+      image_data[pixel_index + 1] = red_value; // G
+      image_data[pixel_index + 2] = 0; // B
+      image_data[pixel_index + 3] = 255; // A
+      index += 1;
+    }
+  }
+
+  image_data
+}
+
 fn main() {
-    println!("Hello, world!");
+  let (file_path, output_name) = get_args();
+  let binary_data = get_file_as_binary(&file_path);
+  let (width, height) = get_dimensions_from_len(binary_data.len() as f64);
+  let mut file_handler = fs::File::create(output_name).unwrap();
+  let values_per_pixel = 4;
+
+  let image_data = fill_image_data(
+    &binary_data,
+    width as i32,
+    height as i32,
+    values_per_pixel as i32,
+  );
+
+  match png_encode_mini::write_rgba_from_u8(&mut file_handler, &image_data, width, height) {
+    Ok(_) => println!("Image written"),
+    Err(e) => println!("Error {:?}", e),
+  }
+
+  println!("data len: {}, width: {}, height: {}, width*height: {}", binary_data.len(), width, height, width * height);
 }
