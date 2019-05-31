57027b816645df89ecd9ff0a17db0152e0ffcdeb
nikita-skobov <nskobov.websites@gmail.com>
2019-05-31 16:13:03 -0500

    exposes publically other modules in image_data dir

--- src/image_data/mod.rs
+++ src/image_data/mod.rs
@@ -2,6 +2,8 @@ mod get_image_data;
 mod pixels;
 
 pub use get_image_data::get_image_data;
+pub use get_image_data::get_image_data_from_sprites;
+pub use pixels::Pixel;
 
 pub struct Sprite {
     x_offset: i32,
