c599b868ca7905e4a17970e0f6f5844d2072cbff
nikita-skobov <nskobov.websites@gmail.com>
2019-05-31 16:19:01 -0500

    rewrites main to use sprite list to have resizable pixel sprites

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,32 @@ fn main() {
     let binary_data = helpers::get_file_as_binary(&file_path);
     let (width, height) = helpers::get_dimensions_from_len(binary_data.len() as f64);
     let mut file_handler = fs::File::create(output_name).unwrap();
-    let values_per_pixel = 4;
 
-    let image_data = image_data::get_image_data(
-        &binary_data,
-        width as i32,
-        height as i32,
-        values_per_pixel as i32,
-    );
+    let pixel_size = 3;
+    let mut sprite_list: Vec<image_data::Sprite> = Vec::new();
+    let total_width: i32 = width as i32 * pixel_size;
+    let total_height: i32 = height as i32 * pixel_size;
 
-    match png_encode_mini::write_rgba_from_u8(&mut file_handler, &image_data, width, height) {
+    let mut xoff = 0;
+    let mut yoff = 0;
+    for byte in binary_data {
+        let color = image_data::Pixel::new(0, byte, 0, 255);
+        let big_pixel = image_data::Sprite::new(xoff, yoff, pixel_size, pixel_size, &color);
+        sprite_list.push(big_pixel);
+
+        xoff += pixel_size;
+        if xoff > total_width {
+            xoff = 0;
+            yoff += pixel_size;
+        }
+    }
+
+
+
+    let image_data = image_data::get_image_data_from_sprites(sprite_list, total_width, total_height);
+
+    match png_encode_mini::write_rgba_from_u8(&mut file_handler, &image_data, total_width as u32, total_height as u32) {
         Ok(_) => println!("Image written"),
         Err(e) => println!("Error {:?}", e),
     }
-
-    println!(
-        "data len: {}, width: {}, height: {}, width*height: {}",
-        binary_data.len(),
-        width,
-        height,
-        width * height
-    );
 }
