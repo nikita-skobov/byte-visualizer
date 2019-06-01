2f66f9081e52495e9e5412f57572bbb6a9d0bb65
nikita-skobov <nskobov.websites@gmail.com>
2019-05-31 21:53:23 -0500

    adds module exposing to png library

--- src/image_data/mod.rs
+++ src/image_data/mod.rs
@@ -1,7 +1,10 @@
 mod get_image_data;
 mod pixels;
 mod sprite;
+mod png;
 
+pub use png::crc32;
+pub use png::write_rgba_from_u8;
 pub use get_image_data::get_image_data;
 pub use get_image_data::get_image_data_from_sprites;
 pub use pixels::Pixel;
